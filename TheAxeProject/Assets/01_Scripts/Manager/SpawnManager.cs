0117e2248219b2cbe226daf9d8cc5977d7feb458
MK <minrim0615@gmail.com>
2024-11-23 23:01:52 +0900

    Adustment Enemy and Spawn Balance

--- TheAxeProject/Assets/01_Scripts/Manager/SpawnManager.cs
+++ TheAxeProject/Assets/01_Scripts/Manager/SpawnManager.cs
@@ -10,45 +10,35 @@ public class SpawnManager : MonoSingleton<SpawnManager>
 {
     public List<PoolTypeSO> enemyPool = new List<PoolTypeSO>();
     public List<Transform> SpawnPoint = new List<Transform>();
-
+    public bool IsWave { get; set; } = false;
+    
     [SerializeField] private float _spawnTime = 2.5f;
-    [SerializeField] private float _spawnDecreaseTime = 0.3f;
+    [SerializeField] private float _spawnDecreaseTime = 0.1f;
+    [SerializeField] private float _spawnDecreaseCooldown = 30f;
     [SerializeField] private float _waveCooldown = 60f;
-    [SerializeField] private int _waveCount = 10;
-
-    public bool IsWave { get; set; } = true;
+    [SerializeField] private int _waveCount = 50;
     
     private PoolManagerSO _poolManager;
     private bool _isStopSpawn = false;
     private bool _isStartSpawn = false;
 
-    private float _time;
-
     private void Start()
     {
-        _time = _waveCooldown;
         _poolManager = SingletonPoolManager.Instance.GetPoolManager(PoolEnumType.Enemy);
         SpawnStart();
     }
 
     public void SpawnStart()
     {
+        StartSpawn();
         StartCoroutine(SpawnCoroutine());
+        StartCoroutine(SpawnDecreaseTime());
+        StartCoroutine(WaveTime());
     }
 
     private void Update()
     {
-        if (_isStartSpawn)
-        {
-            _time -= Time.deltaTime;
-
-            if (_time <= 0)
-            {
-                _spawnTime -= _spawnDecreaseTime;
-                _time = _waveCooldown;
-            }
-        }
-        
+        // TODO : IsStopSpawn 바꾸기 
         if (Input.GetKeyDown(KeyCode.T))
         {
             if (_isStopSpawn)
@@ -58,28 +48,76 @@ public class SpawnManager : MonoSingleton<SpawnManager>
         }
     }
 
+    private PoolTypeSO RandomEnemyType()
+    {
+        // 1. 근거리
+        // 2. 돌진
+        // 3. 원거리
+
+        int rand = Random.Range(0, 101);
+
+        // 근거리 50%
+        if (rand < 50)
+        {
+            return enemyPool[0];
+        }
+        // 돌진 30%
+        else if (rand < 80)
+        {
+            return enemyPool[1];
+        }
+        // 원거리 20%
+        else
+        {
+            return enemyPool[2];
+        }
+    }
+
+    private void StartSpawn()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            Enemy enemy = _poolManager.Pop(RandomEnemyType()) as Enemy;
+            enemy.transform.position = SpawnPoint[Random.Range(0, SpawnPoint.Count)].position;
+        }
+    }
+
+    private void EnemyWave()
+    {
+        //Vector2 spawnPoints = SpawnPoint[Random.Range(0, SpawnPoint.Count)].position;
+        for (int i = 0; i < _waveCount; ++i)
+        {
+            Enemy enemy = _poolManager.Pop(RandomEnemyType()) as Enemy;
+            enemy.transform.position = SpawnPoint[Random.Range(0, SpawnPoint.Count)].position;
+        }
+
+        IsWave = false;
+    }
+
+    private IEnumerator WaveTime()
+    {
+        if (_isStopSpawn) yield return null;
+        
+        yield return new WaitForSeconds(_waveCooldown);
+        EnemyWave();
+    }
+
+    private IEnumerator SpawnDecreaseTime()
+    {
+        if (_isStopSpawn) yield return null;
+        
+        yield return new WaitForSeconds(_spawnDecreaseCooldown);
+        _spawnTime -= _spawnDecreaseTime;
+    }
+    
     private IEnumerator SpawnCoroutine()
     {
         _isStartSpawn = true;
         while (_isStopSpawn == false)
         {
-            if (IsWave)
-            {
-                Vector2 spawnPoints = SpawnPoint[Random.Range(0, SpawnPoint.Count)].position;
-                for (int i = 0; i < _waveCount; ++i)
-                {
-                    Enemy enemy = _poolManager.Pop(enemyPool[Random.Range(0, enemyPool.Count)]) as Enemy;
-                    enemy.transform.position = spawnPoints;
-                }
-
-                IsWave = false;
-            }
-            else
-            {
-                Enemy enemy = _poolManager.Pop(enemyPool[Random.Range(0, enemyPool.Count)]) as Enemy;
-                enemy.transform.position = SpawnPoint[Random.Range(0, SpawnPoint.Count)].position;
-                
-            }
+            Enemy enemy = _poolManager.Pop(RandomEnemyType()) as Enemy;
+            enemy.transform.position = SpawnPoint[Random.Range(0, SpawnPoint.Count)].position;
+            
             yield return new WaitForSeconds(_spawnTime);
         }
         
